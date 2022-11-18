04d92621f84c5df3493d89dbd0567ce03a4a673c
JuliaKimba <kimbaeva@gmail.com>
2022-11-18 22:10:38 +0300

    edit form

--- script/content.js
+++ script/content.js
@@ -2,7 +2,7 @@ const openModalAdd = document.querySelector('[data-open-modal]')
 const modalsWr = document.querySelector('[data-modal_wr]')
 
 const items = document.querySelector('.items');
-const stor = localStorage.getItem('data')
+// const stor = localStorage.getItem('data')
 
 class Api {
    constructor(url) {
@@ -88,10 +88,10 @@ const generateHTMLForCat = (cat) => `
    <div class='info__cat'>
       <div class='info__name-id'>
       <div class='cat__name'>${cat.name}</div>
-      <div class='cat__id'>ID ${cat.id}</div>
+      <div class='cat__id'>ID №${cat.id}</div>
       </div>
-      <div class='cat__age'>Возраст ${cat.id}</div>
-      <div class='cat__rate'>Рейтинг ${cat.rate}</div>
+      <div class='cat__age'>Возраст: ${cat.age}</div>
+      <div class='cat__rate'>Рейтинг: ${cat.rate}</div>
    </div>
    <div class="edit__container">
       <button data-action='show' class="edit__cat">Подробнее</button>
@@ -105,22 +105,22 @@ const generateModalShowCat = (cat) => `
 <div data-id_card='${cat.id}' class='modal__container'>
    <div class='modal__show'>
       <div class="item__img">
-         <img src="${cat.img_link}" alt="">
+         <img src="${cat.img_link}" alt="${cat.name}">
       </div>
       <div class='modal__description'>
          <div class='show__info-cat'>
          <div class='info__name-id'>
             <div class='cat__name'>${cat.name}</div>
-            <div class='cat__id'>ID ${cat.id}</div>
+            <div class='cat__id'>id ${cat.id}</div>
             </div>
                <div class='pharam'>   
-               <div class='cat__age'>Возраст ${cat.id}</div>
-                  <div class='cat__rate'>Рейтинг ${cat.rate}</div>
+               <div class='cat__age'>Возраст: ${cat.id}</div>
+                  <div class='cat__rate'>Рейтинг: ${cat.rate}</div>
                </div>
          <div class='cat__description'>${cat.description}</div>
       </div>
       <div class="edit__container">
-         <button data-open-put data-action='edit' class="edit__info-cat">Редактировать</button>
+         <button data-open-put data-action='edit' class="edit__info-cat edit__cat">Редактировать</button>
          <button data-action='delete' class="delete__cat">Удалить</button>
       </div>
       </div>
@@ -154,47 +154,115 @@ items.addEventListener('click', (e) => {
 
 
 
+const inputId = document.querySelector('#inputId')
+const inputName = document.querySelector('#inputName')
+const inputAge = document.querySelector('#inputAge')
+const inputRate = document.querySelector('#inputRate')
+const inputDescription = document.querySelector('#inputDescription')
+const inputLink = document.querySelector('#inputLinkImg')
+
 document.forms.add_cat.addEventListener('submit', (e) => {
    e.preventDefault()
 
    const data = Object.fromEntries(new FormData(e.target).entries())
-   console.log(data);
+   console.log({ data });
+
    data.id = Number(data.id)
    data.rate = Number(data.rate)
    data.age = Number(data.age)
    data.favorite = data.favorite == 'on'
+   // let name = data.nameCat
+   // data.name
+   // delete data.nameCat  
 
-   const inputId = document.querySelector('#inputId')
-   const inputName = document.querySelector('#inputName')
-   const inputAge = document.querySelector('#inputAge')
-   const inputRate = document.querySelector('#inputRate')
-   const inputDescription = document.querySelector('#inputDescription')
-   const inputLink = document.querySelector('#inputLinkImg') 
+   // const inputId = document.querySelector('#inputId')
+   // const inputName = document.querySelector('#inputName')
+   // const inputAge = document.querySelector('#inputAge')
+   // const inputRate = document.querySelector('#inputRate')
+   // const inputDescription = document.querySelector('#inputDescription')
+   // const inputLink = document.querySelector('#inputLinkImg') 
+
+
+   if (inputLink.value == '') {
+      data.img_link = 'https://raw.githubusercontent.com/GuliaKimba/catsimg/main/imgnew/defaultPhotoCat.jpg'
+   }
 
   
+ 
+
+   let ok;
    api.getAllCats().then((res) => {
-      console.log({res});
-     if(res.data.some(e => e.id === Number(inputId.value))) {
-      inputId.classList.add('stop')
-      
-     }
+
+      res.data.forEach((e) => {
+         if (data.id === e.id) {
+
+            console.log(data.id);
+            console.log(e.id);
+            inputId.classList.add('stop')
+            alert('Такой ID уже существует. Введите другое число!')
+            ok = false
+            console.log({ ok });
+            return
+         } 
+      })
    })
+   console.log({ ok });
+   let b;
+   if (data.name == '') {
+      console.log({inputName});
+      alert ('Введите имя')
+      b = false
+      return
+   }
+   let c;
+   if (data.id == '') {
+      console.log({inputName});
+      alert ('Введите ID')
+      c = false
+      return
+   }
 
-
-     api.addCat(data).then(() => {
-      
-      
+   api.addCat(data).then(() => {
+      if ((ok == false) || (b == false) || (c == false)) {
+         return
+      }  else {
+      console.log({ data });
       items.insertAdjacentHTML('beforeend', generateHTMLForCat(data))
       modalsWr.classList.add('hidden__modal')
+      console.log({ modalsWr });
       e.target.reset()
-
+      }
    }).catch(alert)
 
-   
 
-   
+   // api.addCat(data).then(() => {
+   //    console.log({ data });
+   //    items.insertAdjacentHTML('beforeend', generateHTMLForCat(data))
+   //    modalsWr.classList.add('hidden__modal')
+   //    console.log({ modalsWr });
+   //    e.target.reset()
+
+   // }).catch(alert)
+
+
 
 })//конец формы
+
+
+document.forms.add_cat.addEventListener('input', (e) => {
+   const formDataCat = Object.fromEntries(new FormData(document.forms.add_cat).entries());
+   const dataSetLS = localStorage.setItem(document.forms.add_cat.name, JSON.stringify(formDataCat))
+
+})
+
+const dataGetLS = localStorage.getItem(document.forms.add_cat.name)
+const dataGetObjLS = dataGetLS ? JSON.parse(dataGetLS) : undefined
+
+if (dataGetObjLS) {
+   Object.keys(dataGetObjLS).forEach(k => {
+      document.forms.add_cat[k].value = dataGetObjLS[k]
+   })
+}
 
 const closeModalAdd = document.querySelector('.modal__img-add')
 closeModalAdd.addEventListener('click', (e) => {
@@ -218,13 +286,19 @@ items.addEventListener('click', (e) => {
             items.insertAdjacentHTML('beforeend', generateModalShowCat(res.data))
 
             const closeModal = document.querySelector('[data-close-modal]')
-            console.log({closeModal});
+
             closeModal.addEventListener('click', (e) => {
 
                closeModalWindow(e)
 
             })
 
+            const closeModalCntShowCat = document.querySelector('.modal__container')
+            closeModalCntShowCat.addEventListener('click', (e) => {
+               closeModalContainer(e)
+            })
+           
+         
          })
 
          break;
@@ -253,13 +327,13 @@ items.addEventListener('click', (e) => {
       modalPut.classList.remove('hidden__modal')
 
       generateNewForm(idCat)
-     
+
       const closeModal = document.querySelector('.modal_img-sava-edit')
-      console.log(closeModal);
+
       closeModal.addEventListener('click', (e) => {
-   
+
          closeModalWindowEdit(e)
-   
+
       })
 
       document.forms.edit_cat.addEventListener('submit', (e) => {
@@ -277,13 +351,14 @@ items.addEventListener('click', (e) => {
          data.rate = Number(data.rate)
          data.age = Number(data.age)
          data.favorite = data.favorite == 'on'
-         console.log(data);
+
 
          api.showCat(idCat).then((res) => {
             console.log(res.data.age);
             if (ageVal === '') {
                data.age = res.data.age
             }
+            console.log(data.age);
             if (rateVal === '') {
                data.rate = res.data.rate
             }
@@ -294,20 +369,21 @@ items.addEventListener('click', (e) => {
                data.img_link = res.data.img_link
             }
 
+            console.log(data);
 
 
             api.putCat(data, idCat).then(() => {
-
+               console.log({ data });
             }).catch(alert)
 
          })
-         
+
 
          //конец формы
       })
-      
+
    }
-   
+
 
    function generateNewForm(idCat) {
       cardItem.classList.add('hidden__modal');
@@ -343,6 +419,19 @@ function closeModalWindowEdit(e) {
       e.target.closest('[data-modal_put]').classList.add('hidden__modal')
    }
    location.reload()
+
+}
+
+function closeModalContainer(e) {
+   if ((!e.target.classList.contains('.modal__show')) ) {
+      return
+   }else {
+      const parentEl = document.querySelector('.modal__container')
+      
+      parentEl.classList.add('hidden__modal')
+      
+   }
+   // location.reload()
 
 }
 // deleteCat(delCat)
