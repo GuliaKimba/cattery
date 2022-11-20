7970a032c7adf1ff75988e7785a68904b5ed7530
JuliaKimba <kimbaeva@gmail.com>
2022-11-20 16:19:21 +0300

    edit form

--- content.js
+++ content.js
@@ -1,8 +1,7 @@
 const openModalAdd = document.querySelector('[data-open-modal]')
 const modalsWr = document.querySelector('[data-modal_wr]')
-
 const items = document.querySelector('.items');
-// const stor = localStorage.getItem('data')
+
 
 class Api {
    constructor(url) {
@@ -102,7 +101,8 @@ const generateHTMLForCat = (cat) => `
    
 `
 const generateModalShowCat = (cat) => `
-<div data-id_card='${cat.id}' class='modal__container'>
+
+<div data-id_card='${cat.id}' class='modal__container'> 
    <div class='modal__show'>
       <div class="item__img">
          <img src="${cat.img_link}" alt="${cat.name}">
@@ -132,7 +132,6 @@ const api = new Api('http://sb-cats.herokuapp.com/api/2/guliakimba');
 api.getAllCats().then((res) => res.data.forEach(cat => items.insertAdjacentHTML('beforeend', generateHTMLForCat(cat))))
 
 
-
 items.addEventListener('click', (e) => {
 
    switch (e.target.dataset.action) {
@@ -141,7 +140,7 @@ items.addEventListener('click', (e) => {
          const idCat = cardItem.dataset.id_card
          api.deleteCat(idCat).then(() => {
             cardItem.remove()
-            location.reload();
+            // location.reload();
          })
 
          break
@@ -149,9 +148,8 @@ items.addEventListener('click', (e) => {
       default:
          break
    }
+   
 })
-
-
 
 
 const inputId = document.querySelector('#inputId')
@@ -171,24 +169,12 @@ document.forms.add_cat.addEventListener('submit', (e) => {
    data.rate = Number(data.rate)
    data.age = Number(data.age)
    data.favorite = data.favorite == 'on'
-   // let name = data.nameCat
-   // data.name
-   // delete data.nameCat  
-
-   // const inputId = document.querySelector('#inputId')
-   // const inputName = document.querySelector('#inputName')
-   // const inputAge = document.querySelector('#inputAge')
-   // const inputRate = document.querySelector('#inputRate')
-   // const inputDescription = document.querySelector('#inputDescription')
-   // const inputLink = document.querySelector('#inputLinkImg') 
 
 
    if (inputLink.value == '') {
       data.img_link = 'https://raw.githubusercontent.com/GuliaKimba/catsimg/main/imgnew/defaultPhotoCat.jpg'
    }
 
-  
- 
 
    let ok;
    api.getAllCats().then((res) => {
@@ -203,21 +189,21 @@ document.forms.add_cat.addEventListener('submit', (e) => {
             ok = false
             console.log({ ok });
             return
-         } 
+         }
       })
    })
    console.log({ ok });
    let b;
    if (data.name == '') {
-      console.log({inputName});
-      alert ('Введите имя')
+      console.log({ inputName });
+      alert('Введите имя')
       b = false
       return
    }
    let c;
    if (data.id == '') {
-      console.log({inputName});
-      alert ('Введите ID')
+      console.log({ inputName });
+      alert('Введите ID')
       c = false
       return
    }
@@ -225,29 +211,21 @@ document.forms.add_cat.addEventListener('submit', (e) => {
    api.addCat(data).then(() => {
       if ((ok == false) || (b == false) || (c == false)) {
          return
-      }  else {
-      console.log({ data });
-      items.insertAdjacentHTML('beforeend', generateHTMLForCat(data))
-      modalsWr.classList.add('hidden__modal')
-      console.log({ modalsWr });
-      e.target.reset()
+      } else {
+         console.log({ data });
+         items.insertAdjacentHTML('beforeend', generateHTMLForCat(data))
+         modalsWr.classList.add('hidden__modal')
+         console.log({ modalsWr });
+         e.target.reset()
       }
    }).catch(alert)
 
 
-   // api.addCat(data).then(() => {
-   //    console.log({ data });
-   //    items.insertAdjacentHTML('beforeend', generateHTMLForCat(data))
-   //    modalsWr.classList.add('hidden__modal')
-   //    console.log({ modalsWr });
-   //    e.target.reset()
+})
 
-   // }).catch(alert)
-
-
-
-})//конец формы
-
+document.querySelector('.modal__container').addEventListener('click', (e) => {
+   closeModalWindowAdd(e)
+})
 
 document.forms.add_cat.addEventListener('input', (e) => {
    const formDataCat = Object.fromEntries(new FormData(document.forms.add_cat).entries());
@@ -264,16 +242,9 @@ if (dataGetObjLS) {
    })
 }
 
-const closeModalAdd = document.querySelector('.modal__img-add')
-closeModalAdd.addEventListener('click', (e) => {
-   console.log({ e });
-   closeModalWindowAdd(e)
-})
-
 
 items.addEventListener('click', (e) => {
 
-   //show
    switch (e.target.dataset.action) {
       case 'show': {
          const cardItem = e.target.closest('[data-id_card]');
@@ -281,24 +252,15 @@ items.addEventListener('click', (e) => {
 
          let a;
          api.showCat(idCat).then((res) => {
-            // console.log(res);
-
+            
             items.insertAdjacentHTML('beforeend', generateModalShowCat(res.data))
-
-            const closeModal = document.querySelector('[data-close-modal]')
-
-            closeModal.addEventListener('click', (e) => {
-
-               closeModalWindow(e)
-
-            })
 
             const closeModalCntShowCat = document.querySelector('.modal__container')
             closeModalCntShowCat.addEventListener('click', (e) => {
                closeModalContainer(e)
             })
-           
-         
+
+
          })
 
          break;
@@ -307,7 +269,8 @@ items.addEventListener('click', (e) => {
          break
    }
 
-
+   closeModalWindow(e)
+   
    const cardItem = e.target.closest('[data-id_card]');
    const idCat = cardItem.dataset.id_card
 
@@ -378,8 +341,6 @@ items.addEventListener('click', (e) => {
 
          })
 
-
-         //конец формы
       })
 
    }
@@ -389,9 +350,7 @@ items.addEventListener('click', (e) => {
       cardItem.classList.add('hidden__modal');
    }
 
-   //конец обработчик показать конкретного кота
 })
-
 
 
 openModalAdd.addEventListener('click', () => {
@@ -399,39 +358,34 @@ openModalAdd.addEventListener('click', () => {
 })
 
 
-
 function closeModalWindow(e) {
    if (e.target.classList.contains('.modal__img') || e.target.closest('.modal__img')) {
-      e.target.closest('[data-id_card]').classList.add('hidden__modal')
+      console.log("закрытие модальки");
+      e.target.closest('.modal__container').classList.add('hidden__modal')
    }
-   location.reload()
-
 }
 function closeModalWindowAdd(e) {
-   if (e.target.classList.contains('.modal__img-1') || e.target.closest('.modal__img-1')) {
-      e.target.closest('[data-id_card-1]').classList.add('hidden__modal')
+   if (e.target.classList.contains('.modal__img-add') || e.target.closest('.modal__img-add')) {
+      e.target.closest('.modal__container').classList.add('hidden__modal')
    }
-   location.reload()
+
 
 }
 function closeModalWindowEdit(e) {
    if (e.target.classList.contains('.modal__img-add') || e.target.closest('.modal__img-add')) {
       e.target.closest('[data-modal_put]').classList.add('hidden__modal')
    }
-   location.reload()
 
 }
 
 function closeModalContainer(e) {
-   if ((!e.target.classList.contains('.modal__show')) ) {
+   if ((!e.target.classList.contains('.modal__show'))) {
       return
-   }else {
+   } else {
       const parentEl = document.querySelector('.modal__container')
-      
+
       parentEl.classList.add('hidden__modal')
-      
+
    }
-   // location.reload()
 
 }
-// deleteCat(delCat)
